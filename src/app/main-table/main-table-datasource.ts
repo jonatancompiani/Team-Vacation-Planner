8de78708fcca9790741072ff5f959cc088b53d54
Jonatan Compiani <jonatan.compiani@farfetch.com>
2023-02-15 22:52:30 +0000

    Implemented dynamic generation of dates

--- src/app/main-table/main-table-datasource.ts
+++ src/app/main-table/main-table-datasource.ts
@@ -6,33 +6,48 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 
 // TODO: Replace this with your own data model type
 export interface MainTableItem {
-  name: string;
   id: number;
+  month: string;
+  sun1: string | undefined;
+  mon1: string | undefined;
+  tue1: string | undefined;
+  wed1: string | undefined;
+  thu1: string | undefined;
+  fri1: string | undefined;
+  sat1: string | undefined;
+  sun2: string | undefined;
+  mon2: string | undefined;
+  tue2: string | undefined;
+  wed2: string | undefined;
+  thu2: string | undefined;
+  fri2: string | undefined;
+  sat2: string | undefined;
+  sun3: string | undefined;
+  mon3: string | undefined;
+  tue3: string | undefined;
+  wed3: string | undefined;
+  thu3: string | undefined;
+  fri3: string | undefined;
+  sat3: string | undefined;
+  sun4: string | undefined;
+  mon4: string | undefined;
+  tue4: string | undefined;
+  wed4: string | undefined;
+  thu4: string | undefined;
+  fri4: string | undefined;
+  sat4: string | undefined;
+  sun5: string | undefined;
+  mon5: string | undefined;
+  tue5: string | undefined;
+  wed5: string | undefined;
+  thu5: string | undefined;
+  fri5: string | undefined;
+  sat5: string | undefined;
+  sun6: string | undefined;
+  mon6: string | undefined;
 }
 
-// TODO: replace this with real data from your application
-const EXAMPLE_DATA: MainTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
-];
+const MONTHS: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 /**
  * Data source for the MainTable view. This class should
@@ -40,7 +55,7 @@ const EXAMPLE_DATA: MainTableItem[] = [
  * (including sorting, pagination, and filtering).
  */
 export class MainTableDataSource extends DataSource<MainTableItem> {
-  data: MainTableItem[] = EXAMPLE_DATA;
+  data: MainTableItem[] | undefined;
   paginator: MatPaginator | undefined;
   sort: MatSort | undefined;
 
@@ -54,16 +69,10 @@ export class MainTableDataSource extends DataSource<MainTableItem> {
    * @returns A stream of the items to be rendered.
    */
   connect(): Observable<MainTableItem[]> {
-    if (this.paginator && this.sort) {
-      // Combine everything that affects the rendered data into one update
-      // stream for the data-table to consume.
-      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
-        .pipe(map(() => {
-          return this.getPagedData(this.getSortedData([...this.data ]));
-        }));
-    } else {
-      throw Error('Please set the paginator and sort on the data source before connecting.');
-    }
+    return merge(observableOf(this.data))
+      .pipe(map(() => {
+        return this.getDates();
+      }));
   }
 
   /**
@@ -72,40 +81,48 @@ export class MainTableDataSource extends DataSource<MainTableItem> {
    */
   disconnect(): void {}
 
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getPagedData(data: MainTableItem[]): MainTableItem[] {
-    if (this.paginator) {
-      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-      return data.splice(startIndex, this.paginator.pageSize);
-    } else {
-      return data;
-    }
-  }
 
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getSortedData(data: MainTableItem[]): MainTableItem[] {
-    if (!this.sort || !this.sort.active || this.sort.direction === '') {
-      return data;
-    }
+  private getDates(){
+    var data: MainTableItem[] = [];
 
-    return data.sort((a, b) => {
-      const isAsc = this.sort?.direction === 'asc';
-      switch (this.sort?.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
-        default: return 0;
+    var currentYear = new Date().getFullYear();
+    var firstDayOfYear = new Date(currentYear, 0, 1);
+    var lastDayOfYear = new Date(currentYear, 12, 31);
+
+    var propNames: string[] | undefined;
+
+    var monthToAdd: MainTableItem | undefined;
+    var propIndex: number = 0.
+    for (var d = firstDayOfYear; d <= lastDayOfYear; d.setDate(d.getDate() + 1)) {
+      if (d.getDate() == 1){
+        if (!!monthToAdd){
+          data.push(monthToAdd);
+        }
+        monthToAdd = { id: d.getMonth(), month: MONTHS[d.getMonth()], sun1: undefined, mon1: undefined, tue1: undefined, wed1: undefined, thu1: undefined, fri1: undefined, sat1: undefined, sun2: undefined, mon2: undefined, tue2: undefined, wed2: undefined, thu2: undefined, fri2: undefined, sat2: undefined, sun3: undefined, mon3: undefined, tue3: undefined, wed3: undefined, thu3: undefined, fri3: undefined, sat3: undefined, sun4: undefined, mon4: undefined, tue4: undefined, wed4: undefined, thu4: undefined, fri4: undefined, sat4: undefined, sun5: undefined, mon5: undefined, tue5: undefined, wed5: undefined, thu5: undefined, fri5: undefined, sat5: undefined, sun6: undefined, mon6: undefined };
+      
+        if (!propNames){
+          propNames = Object.getOwnPropertyNames(monthToAdd);
+        }
+      
+        propIndex = propNames.indexOf(this.getPropFromDayOfWeek(d))-1;
       }
-    });
-  }
-}
+      if(propNames){
+        propIndex += 1;
+        var propName = propNames[propIndex];
+      
+        if(monthToAdd)
+          monthToAdd[propName as keyof MainTableItem] = d.getDate() as never;
+      }
+    }
 
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-function compare(a: string | number, b: string | number, isAsc: boolean): number {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    return data;
+  }
+
+  private getPropFromDayOfWeek(date: Date): keyof MainTableItem{
+
+    var propNames = ["sun1", "mon1", "tue1", "wed1", "thu1", "fri1", "sat1"];
+
+    return propNames[date.getDay()] as keyof MainTableItem;
+  }
+
 }
--- src/app/main-table/main-table.component.ts
+++ src/app/main-table/main-table.component.ts
@@ -7,7 +7,7 @@ import { MainTableDataSource, MainTableItem } from './main-table-datasource';
 @Component({
   selector: 'app-main-table',
   templateUrl: './main-table.component.html',
-  styleUrls: ['./main-table.component.css']
+  styleUrls: ['./main-table.component.sass']
 })
 export class MainTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -16,7 +16,46 @@ export class MainTableComponent implements AfterViewInit {
   dataSource: MainTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = [
+    'month',
+    'sun1',
+    'mon1',
+    'tue1',
+    'wed1',
+    'thu1',
+    'fri1',
+    'sat1',
+    'sun2',
+    'mon2',
+    'tue2',
+    'wed2',
+    'thu2',
+    'fri2',
+    'sat2',
+    'sun3',
+    'mon3',
+    'tue3',
+    'wed3',
+    'thu3',
+    'fri3',
+    'sat3',
+    'sun4',
+    'mon4',
+    'tue4',
+    'wed4',
+    'thu4',
+    'fri4',
+    'sat4',
+    'sun5',
+    'mon5',
+    'tue5',
+    'wed5',
+    'thu5',
+    'fri5',
+    'sat5',
+    'sun6',
+    'mon6'
+  ];
 
   constructor() {
     this.dataSource = new MainTableDataSource();
