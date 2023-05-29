3a4bcabb6211f9dd6e87fcf09fa9909d06fcad95
cesarag92 <educador.cesar@gmail.com>
2023-05-29 16:12:56 +0100

    - Adjusted planner creation to use a ngFor - Configure TsConfig to not validate libs before building solution

--- src/app/main-table/main-table.component.ts
+++ src/app/main-table/main-table.component.ts
@@ -10,47 +10,14 @@ import { DayData, MainTableDataSource, MainTableItem } from './main-table-dataso
 export class MainTableComponent implements AfterViewInit {
   @ViewChild(MatTable) table!: MatTable<MainTableItem>;
   dataSource: MainTableDataSource;
-
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = [
-    'month',
-    'sun1',
-    'mon1',
-    'tue1',
-    'wed1',
-    'thu1',
-    'fri1',
-    'sat1',
-    'sun2',
-    'mon2',
-    'tue2',
-    'wed2',
-    'thu2',
-    'fri2',
-    'sat2',
-    'sun3',
-    'mon3',
-    'tue3',
-    'wed3',
-    'thu3',
-    'fri3',
-    'sat3',
-    'sun4',
-    'mon4',
-    'tue4',
-    'wed4',
-    'thu4',
-    'fri4',
-    'sat4',
-    'sun5',
-    'mon5',
-    'tue5',
-    'wed5',
-    'thu5',
-    'fri5',
-    'sat5',
-    'sun6',
-    'mon6'
+  displayedColumns: string[] = [
+    'month', 'sun1', 'mon1', 'tue1', 'wed1', 'thu1', 'fri1', 'sat1', 
+    'sun2', 'mon2', 'tue2', 'wed2', 'thu2', 'fri2', 'sat2', 
+    'sun3', 'mon3', 'tue3', 'wed3', 'thu3', 'fri3', 'sat3', 
+    'sun4', 'mon4', 'tue4', 'wed4', 'thu4', 'fri4', 'sat4',
+    'sun5', 'mon5', 'tue5', 'wed5', 'thu5', 'fri5', 'sat5', 
+    'sun6', 'mon6'
   ];
 
   constructor(private ds: MainTableDataSource) {
@@ -119,7 +86,6 @@ export class MainTableComponent implements AfterViewInit {
   }
 
   toggleEvent(day: DayData) {
-
     // only allow modificartion if a team member is selected
     if(!!this.dataSource.selectedMember && !!this.dataSource.selectedMemberId){
 
